9cf81b592a38b2d5fc6785af460e9326436ee3ee
Muhammad Hafif Al Busyro <muhammadhafifalbusyro@gmail.com>
2023-11-04 14:53:10 +0700

    pertemuan 2

---
+++ Pertemuan2/Tugas/tugas.js
@@ -0,0 +1,11 @@
+//No.1
+//Buatlah 5 variable dengan nama variable hewan, assignkan nama hewannya apa menggunakan tipe data string bebas, lalu tampilkan output dari variable hewan tersebut
+
+//No2
+//Buatlah kasus jika menggunakan nama variable yang sama , maka akan muncul output nilai yang berbeda, jelaskan detail perline nya 
+
+//No3 
+//Buatlah contoh perbedaan var, let , const
+
+//No4
+//Buatlah contoh 5 penggunaan operator aritmatika
---
+++ Pertemuan2/operator.js
@@ -0,0 +1,30 @@
+// Operator
+// Simbol untuk mengoperasikan sesuatu pada nilai/ variabel
+
+//Binary => mengoperasikan 2 nilai 
+// Ternary => mengoperasikan 3 nilai
+// Unary => mengoperasikan 1 nilai
+
+// 1. Operator Aritmatika
+// Sistem operasi pada matematika
+// + - * / %
+// Kukabataku
+
+// let nilai1 = 20
+// let nilai2 = 30
+// let nilai3 = 10
+
+// let jumlah = nilai1+ nilai3
+// let pembagian = nilai1 / nilai3
+
+// console.log(pembagian)
+
+
+//Modulus
+// Sisa hasil bagi
+//  let nilai1 = 10
+//  let nilai2 = 6
+
+//  let modulus = nilai1 % nilai2
+
+//  console.log(modulus)
